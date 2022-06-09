1bc6d086823fc96a4bdd515c1c40c72474ab17d7
MaksimOnza <Maximus83onza@gmail.com>
2022-06-09 18:19:19 +0300

    small refactoring

--- src/controllers/ControllerLogin.php
+++ src/controllers/ControllerLogin.php
@@ -2,8 +2,6 @@
 namespace SimplyNotes;
 
 use SimplyNotes\DBO;
-//use SimplyNotes\Models\ModelNote;
-//use SimplyNotes\ModelUser;
 use function SimplyNotes\db_sqlite\query_select;
 use function SimplyNotes\render;
 
@@ -12,8 +10,10 @@ $password = $_REQUEST['password'] ?? '';
 
 function match_input($input, $min, $max)
 {
-    if (!preg_match('/^([A-Za-z\d\-]|_(?!_)){' . $min . ',' . $max . '}$/', $input))
+    if (!preg_match('/^([A-Za-z\d\-]|_(?!_)){' . $min . ',' . $max . '}$/', $input)) {
         return false;
+    }
+    
     return true;
 }
 
@@ -22,16 +22,21 @@ function valid_input($login, $password)
     if (empty($login) or empty($password)) {
         return false;
     }
+    
     if (!match_input($password, 4, 50) or !match_input($login, 3, 10)) {
         return false;
     }
+    
     return true;
 }
 
 function equals_log_pass($login, $password){
     $user = query_select("SELECT name, password FROM user WHERE name = ?", [1 => $login])[0];
-    if($login == $user['name'] and password_verify($password, $user['password']))
+    
+    if($login == $user['name'] and password_verify($password, $user['password'])) {
         return true;
+    }
+    
     return false;
 }
 
@@ -40,7 +45,8 @@ if (valid_input($login, $password) and equals_log_pass($login, $password)) {
     $notes = query_select("SELECT * FROM notes WHERE id_user = ?", [1 => $user['id']]);
     $_SESSION['id_user'] = $user['id'];
     $_SESSION['user_name'] = $login;
-    //$note = new ModelUser($user['id'], $login, $notes);
+    
     header('Location: /index.php?path=ControllerFirstView');
-}else
+} else {
     return render('login', []);
+}
